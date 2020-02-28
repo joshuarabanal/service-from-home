9a20354f7b516153cdc06062444affa11a6b3f65
joshuarabanal <joshuarabanal@gmail.com>
2020-02-28 16:46:36 -0400

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { ActionBarComponent } from './basic-components/action-bar/action-bar.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, ActionBarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/basic-components/action-bar/action-bar.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, Input, HostListener} from '@angular/core';
+
+@Component({
+  selector: 'action-bar',
+  templateUrl: './action-bar.component.html',
+  styleUrls: ['./action-bar.component.css']
+})
+export class ActionBarComponent implements OnInit {
+  @Input("title") title: string;
+  @Input("action-icon") actionIcon: string[];
+  @Input("disable-fade") disableFade : boolean;
+  @Input("add-spacer") addSpacer: boolean;
+  actionbarClass: string = "transparent";
+  //@Input("menu-item-names") menuNames: string[];
+
+  //@Input("menu-item-links") nemuLinks: string;
+  @HostListener("window:scroll", [])
+    onWindowScroll() {
+      this.updateScroll();
+    }
+    updateScroll(){
+      const number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+      if (number > 100) {
+        this.actionbarClass = "opaque";
+      } else{
+        this.actionbarClass = "transparent";
+      }
+    }
+
+  constructor() {
+      this.updateScroll();
+  }
+
+  ngOnInit() {
+  }
+
+}
